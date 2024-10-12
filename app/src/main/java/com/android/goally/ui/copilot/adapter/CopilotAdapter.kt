957bd8170eaabe52dc91beeea5d94ace2a7cc082
Zohaib Hassan <zohaibu62@gmail.com>
2024-10-12 13:21:45 +0500

    shimmer effect implemented

--- app/src/main/java/com/android/goally/ui/copilot/adapter/CopilotAdapter.kt
+++ app/src/main/java/com/android/goally/ui/copilot/adapter/CopilotAdapter.kt
@@ -5,39 +5,54 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.android.goally.R
-import com.android.goally.data.model.api.response.copilot.Activity
 import com.android.goally.data.model.api.response.copilot.Routine
 import com.android.goally.databinding.RowCopilotBinding
+import com.android.goally.databinding.ShimmerRowCopilotBinding
 import com.android.goally.util.setSafeOnClickListener
 import com.bumptech.glide.Glide
-import kotlin.reflect.KFunction1
 
 class CopilotAdapter(
     private var routines: List<Routine>,
     private val onCopilotClicked: (Routine) -> Unit
-) : RecyclerView.Adapter<CopilotAdapter.CoPilotViewHolder>() {
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoPilotViewHolder {
-        val binding = RowCopilotBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return CoPilotViewHolder(binding)
+    private val VIEW_TYPE_ITEM = 1
+    private val VIEW_TYPE_SHIMMER = 2
+    private var isLoading = true // Track loading state
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if (viewType == VIEW_TYPE_ITEM) {
+            val binding = RowCopilotBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            CoPilotViewHolder(binding)
+        } else {
+            val binding = ShimmerRowCopilotBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            ShimmerViewHolder(binding)
+        }
     }
 
     override fun getItemCount(): Int {
-        return routines.size
+        return if (isLoading) 10 else routines.size // Show 10 shimmer items when loading
     }
 
-    override fun onBindViewHolder(holder: CoPilotViewHolder, position: Int) {
-        val routine = routines[position]
-        routine?.let {
-            holder.bind(it)
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is CoPilotViewHolder) {
+            val routine = routines[position]
+            holder.bind(routine)
+        } else if (holder is ShimmerViewHolder) {
+            // Shimmer view does not need to bind any data
+            holder.startShimmer() // Start shimmer effect
         }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (isLoading) VIEW_TYPE_SHIMMER else VIEW_TYPE_ITEM
     }
 
     fun submitList(newRoutines: List<Routine>) {
         this.routines = newRoutines
+        this.isLoading = false // Set loading to false when data is received
         notifyDataSetChanged()
     }
-
 
     inner class CoPilotViewHolder(private val binding: RowCopilotBinding) :
         RecyclerView.ViewHolder(binding.root) {
@@ -62,6 +77,14 @@ class CopilotAdapter(
             binding.root.setSafeOnClickListener {
                 onCopilotClicked(routine)
             }
+        }
+    }
+
+    inner class ShimmerViewHolder(private val binding: ShimmerRowCopilotBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun startShimmer() {
+            binding.shimmerViewContainer.startShimmer()
         }
     }
 }
